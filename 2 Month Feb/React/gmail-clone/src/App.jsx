94428e6163952b2d7ff6f0c62c0235ac69dbf548
Mayuri02220 <mayurikumbhar2006@email.com>
2026-02-10 19:42:00 +0530

    done

---
+++ 2 Month Feb/React/gmail-clone/src/App.jsx
@@ -0,0 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-center bg-danger">HOC</h1>
+      <p>React allows us to create reusable UI components</p>
+      <p>React allows us to create reusable UI components</p>
+      <p>React allows us to create reusable UI components</p>
+      <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Contact Us</li>
+      </ul>
+    </>
+  )
+}
+
+export default App;
