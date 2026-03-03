f5e28f0a9b57ee72f621be27382c3b60d5ca2f82
Mayuri02220 <mayurikumbhar2006@email.com>
2026-03-03 08:01:34 +0530

    Done

---
+++ 3 Month March/CRUD project/backend/server.js
@@ -0,0 +1,30 @@
+// Backend Project
+
+// Node js
+// Express js
+// DB- MongoDB
+
+
+//API
+
+// 1. API Create Item - get data values from front end (Item details) and store into Database
+
+// 2. API Update Item - get Item Details from Front end which item we need to update
+
+// 3. API Delete Item -  get Item Details from Front end an delete this record from database
+
+// 4. API get all records - get all records from DB and show to UI frontend
+
+
+
+//Arrow funtion
+ 
+const getdata = () => {
+
+}
+
+// Normal function 
+
+function getdata (){
+
+}
