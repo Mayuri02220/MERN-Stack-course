78c2d33c7896657d1ac7618e2b1c63486441df1f
Mayuri02220 <mayurikumbhar2006@email.com>
2026-03-04 20:54:57 +0530

    changes

--- 3 Month March/CRUD project/backend/server.js
+++ 3 Month March/CRUD project/backend/server.js
@@ -1,18 +1,13 @@
 // Backend Project
-
 // Node js
 // Express js
 // DB- MongoDB
 
 
 //API
-
 // 1. API Create Item - get data values from front end (Item details) and store into Database
-
 // 2. API Update Item - get Item Details from Front end which item we need to update
-
 // 3. API Delete Item -  get Item Details from Front end an delete this record from database
-
 // 4. API get all records - get all records from DB and show to UI frontend
 
 //Arrow funtion
@@ -28,7 +23,7 @@ const mongoose = require("mongoose")
 
 app.use(express.json())
 
-mongoose.connect("mongodb://127.0.1:27017///Item-database").then( () => console.log("mongo DB connected") ).catch( (error) => console.log("error") )
+mongoose.connect("mongodb://127.0.0.1:27017///Item-database").then( () => console.log("mongo DB connected") ).catch( (error) => console.log("error") )
 
 //schema-model-database table structure
 const itemsSchema = new mongoose.Schema({
@@ -37,5 +32,6 @@ const itemsSchema = new mongoose.Schema({
     sellingprice : Number
 })
 
-//API
 const Items = new mongoose.model("Items", itemsSchema) //table or collection name
+
+//API
